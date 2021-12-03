23063cf3824e3bcb884033947bb7cc384e513e84
Syakhisk <mail.syakhisk@gmail.com>
2021-12-03 12:55:47 +0700

    yoda
    yoda

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './yoda.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <p> Understand you, I do not. </p>
+          <p> Confuse me, you have. </p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
